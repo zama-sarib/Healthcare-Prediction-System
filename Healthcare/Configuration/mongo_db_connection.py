f9f6902a47f0f1a8558369d427953bc1785e3d3e
Sarib <zama.sarib@gmail.com>
2024-08-19 20:11:15 +0530

    Removed printing of MongoDB Connection String

--- Healthcare/Configuration/mongo_db_connection.py
+++ Healthcare/Configuration/mongo_db_connection.py
@@ -12,7 +12,6 @@ class MongoDBClient:
 
             if MongoDBClient.client is None:
                 mongo_db_url = os.getenv(MONGODB_URL_KEY)
-                print(mongo_db_url)
                 if "localhost" in mongo_db_url:
                     MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
                 else:
