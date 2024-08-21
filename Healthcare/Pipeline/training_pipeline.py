82f5366f0c522778e7df110b01994e2b8aa66975
Sarib <zama.sarib@gmail.com>
2024-08-21 20:16:41 +0530

    Updated Schema.yaml & added data validation

--- Healthcare/Pipeline/training_pipeline.py
+++ Healthcare/Pipeline/training_pipeline.py
@@ -50,16 +50,16 @@ class TrainPipeline:
         except  Exception as e:
             raise  HealthCareException(e,sys)
 
-    # def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
-    #     try:
-    #         data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
-    #         data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
-    #         data_transformation_config=data_transformation_config
-    #         )
-    #         data_transformation_artifact =  data_transformation.initiate_data_transformation()
-    #         return data_transformation_artifact
-    #     except  Exception as e:
-    #         raise  SensorException(e,sys)
+    def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
+        try:
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
+            data_transformation_config=data_transformation_config
+            )
+            data_transformation_artifact =  data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except  Exception as e:
+            raise  HealthCareException(e,sys)
     
     # def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact):
     #     try:
